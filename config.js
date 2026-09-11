72f298bb5304502e1e41131355000c9ac3f1bb6f
dije <djibril.ranggadeja@gmail.com>
2026-09-11 09:23:54 +0800

    add departs

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbwGGVEED8PnJX0_PuM6ekas-2WN-PyVkdG4G2Ph1Q_6Oki6Jw61VRCIlAnNuVlINvfj/exec",
   APP_NAME: "ordersystem-rra",
   COMPANY_NAME: "PT. RRA",
-  DEPARTMENTS: ["welder", "chrome", "polesh", "bubut", "machining", "Admin", "suv", "WS1", "WS KM13"],
+  DEPARTMENTS: ["welder", "chrome", "polesh", "bubut", "machining", "Admin", "suv", "WS1", "WS2", "WS KM13"],
   STATUSES: ["Pending", "In Progress", "Completed", "Cancelled"],
   UNITS: ["pcs", "kg", "meter", "roll", "box", "set", "liter", "pair", "unit", "lot"],
 };
